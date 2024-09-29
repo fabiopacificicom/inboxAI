2f7846b559e4178db3fd15cdde3fd3e1826d299e
fabiopacifici <boolean@fabiopacifici.com>
2024-09-29 13:32:27 +0200

    Update layout files tab title and icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'inboxAI Dashboard') }}</title>
+    <link rel="shortcut icon" href="{{asset('favicon.png')}}" type="image/x-icon">
+
 
     <!-- Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
