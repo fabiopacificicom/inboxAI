9e842a873f9791e2d0d23b39255521edc1ec4b71
fabiopacifici <boolean@fabiopacifici.com>
2024-10-26 17:39:37 +0200

    update Message Card Dialog Component
    - import the necessary dependencies and methods to recreate the component logic
    - add the processMessage method
    - set up settings values for the initial component state
    - add a placeholder method for the component
    - improve message content cleaning process

--- app/Livewire/MessageCardDialog.php
+++ app/Livewire/MessageCardDialog.php
@@ -9,20 +9,47 @@ use App\Traits\Helpers;
 use App\Models\Message;
 use Illuminate\Support\Facades\Cache;
 use Livewire\Attributes\Reactive;
+use App\Traits\Processable;
+use App\Models\Setting;
+use Illuminate\Support\Facades\Log;
 
 class MessageCardDialog extends Component
 {
 
-    use HasMailboxConnection, Helpers;
+    use HasMailboxConnection, Helpers, Processable;
 
 
     public $message;
+    public $settings;
+
+    public function mount()
+    {
+        $this->settings = Setting::all(['key', 'value'])->mapWithKeys(function ($item) {
+            return [$item['key'] => $item['value']];
+        });
+    }
+
 
     public function render()
     {
 
-        return view('livewire.message-card-dialog');
+        $data = [
+            'settings' => $this->settings
+        ];
+        return view('livewire.message-card-dialog', $data);
     }
+
+
+    public function placeholder()
+    {
+        return <<<'HTML'
+        <div>
+            <!-- Loading spinner... -->
+            LOADING ...
+        </div>
+        HTML;
+    }
+
 
 
     /**
@@ -34,7 +61,9 @@ class MessageCardDialog extends Component
     #[On('fetch-message')]
     public function fetchMessage($id): void
     {
+        $this->processingMessages = [];
 
+        $this->message = '';
         //dd($id);
         // get the message to fetch
         $this->message = Message::where('message_identifier', $id)->first();
@@ -52,16 +81,51 @@ class MessageCardDialog extends Component
             $content = $mail?->textHtml;
         }
         //dd($content);
-        $content =  $this->convertHtmlToPlainText($content);
+        $cleanedContent =  preg_replace("/[^A-Za-z0-9 ]/", '', $this->convertHtmlToPlainText($content));
         //dd($content);
         $this->fetching = false;
 
         // find the mesage from the db
         $message = Message::where('message_identifier', $id)->first();
         //update it
-        $message->update(['content' => ($content)]);
+        $message->update(['content' => $cleanedContent]);
 
         // update the messages collection
         //$this->messages = Cache::get('messages', $this->retreiveLatestMessages());
+    }
+
+
+    /**
+     * InboxAI: Process the given message with AI
+     *
+     * This method is responsible of performing classification
+     * and other actions based on the result.
+     *
+     * @param string $messageId the id of the message retrieved from the imap server
+     * @return void
+     */
+    public function processMessage($messageId): void
+    {
+
+        // sets the processing messages to an empty array
+        $this->processingMessages = [];
+
+        // 2. classify the message for further processing
+        [$action, $instructions] = $this->classify($this->message);
+
+        // 4. Perform the actions on the message based on the action
+        $reply = $this->performActions($action, $instructions, $messageId, $this->settings);
+        //dd($reply);
+        $message_content = 'Subject:' . $this->message['subject'] . '. Body: ' . $this->message['content'];
+
+        $data =  [
+            'message_identifier' => $messageId,
+            'message_content' => $message_content,
+            'response_content' => trim(json_decode($reply, true)['reply'])
+        ];
+
+
+        $this->message->replies()->create($data);
+        Log::info("reply created for message $messageId", ['reply' => $reply]);
     }
 }
