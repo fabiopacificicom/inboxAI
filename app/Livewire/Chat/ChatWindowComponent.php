7a6b3561d3689e6bb5ec45990184eee48c45ae35
fabiopacifici <boolean@fabiopacifici.com>
2025-12-18 03:09:46 +0100

    feat(chat): add ChatWindowComponent skeleton

---
+++ app/Livewire/Chat/ChatWindowComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Chat;
+
+use Livewire\Component;
+
+class ChatWindowComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.chat.chat-window-component');
+    }
+}
